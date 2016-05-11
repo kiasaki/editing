2f4eb00932b74d1879947ecec137840e53b9c450
Frederic Gingras <frederic@gingras.cc>
2016-05-11 01:06:35 -0400

    meta: reorganize folders & files

--- main.go
+++ main.go
@@ -4,13 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-
-	"github.com/gdamore/tcell"
-	"github.com/go-errors/errors"
-
-	"github.com/kiasaki/editing/config"
-	"github.com/kiasaki/editing/display"
-	"github.com/kiasaki/editing/text"
 )
 
 const (
@@ -19,26 +12,17 @@ const (
 
 var flagVersion = flag.Bool("version", false, "Show editor's version")
 
+var world *World
+
 func main() {
 	flag.Parse()
 	if *flagVersion {
-		fmt.Println("Editor version ", Version)
+		fmt.Println("Editor version:", Version)
 		os.Exit(0)
 	}
 
-	conf := config.ConfigNew()
-	err := conf.Init()
-	if err != nil {
-		Fatal(err)
-	}
-
-	world := text.WorldNew()
-	world.Init()
-
-	display := display.DisplayNew(conf, world)
-	err = display.Init()
-	if err != nil {
-		display.End()
+	world = NewWorld()
+	if err := world.Init(); err != nil {
 		Fatal(err)
 	}
 
@@ -46,57 +30,9 @@ func main() {
 	defer func() {
 		err := recover()
 		if err != nil {
-			display.End()
-			fmt.Print(errors.Wrap(err, 2).ErrorStack())
 			Fatal(err.(error))
 		}
 	}()
 
-	quit := make(chan struct{})
-
-	// Main loop
-	go func() {
-		for {
-
-			/*
-				DEBUG
-				display.End()
-				spew.Dump(display, world)
-				if true {
-					return
-				}
-			*/
-			display.Redisplay()
-
-			// Now wait for and handle user event
-			ev := display.Screen().PollEvent()
-			switch ev := ev.(type) {
-			case *tcell.EventKey:
-				switch ev.Key() {
-				case tcell.KeyEscape, tcell.KeyEnter, tcell.KeyCtrlQ:
-					close(quit)
-					return
-				default:
-					if ev.Key() == tcell.KeyRune {
-						world.CurrentBuffer().InsertChar(ev.Rune())
-						display.Redisplay()
-					}
-				}
-			case *tcell.EventResize:
-				display.Refresh()
-			}
-		}
-	}()
-
-	<-quit
-
-	err = display.End()
-	if err != nil {
-		Fatal(err)
-	}
-}
-
-func Fatal(err error) {
-	fmt.Fprintf(os.Stderr, "%v\n", err)
-	os.Exit(1)
+	world.Run()
 }
---
+++ v1/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/gdamore/tcell"
+	"github.com/go-errors/errors"
+
+	"github.com/kiasaki/editing/config"
+	"github.com/kiasaki/editing/display"
+	"github.com/kiasaki/editing/text"
+)
+
+const (
+	Version = "Unknown"
+)
+
+var flagVersion = flag.Bool("version", false, "Show editor's version")
+
+func main() {
+	flag.Parse()
+	if *flagVersion {
+		fmt.Println("Editor version ", Version)
+		os.Exit(0)
+	}
+
+	conf := config.ConfigNew()
+	err := conf.Init()
+	if err != nil {
+		Fatal(err)
+	}
+
+	world := text.WorldNew()
+	world.Init()
+
+	display := display.DisplayNew(conf, world)
+	err = display.Init()
+	if err != nil {
+		display.End()
+		Fatal(err)
+	}
+
+	// From now on the screen is initialized so let's handle panics gacefully
+	defer func() {
+		err := recover()
+		if err != nil {
+			display.End()
+			fmt.Print(errors.Wrap(err, 2).ErrorStack())
+			Fatal(err.(error))
+		}
+	}()
+
+	quit := make(chan struct{})
+
+	// Main loop
+	go func() {
+		for {
+
+			/*
+				DEBUG
+				display.End()
+				spew.Dump(display, world)
+				if true {
+					return
+				}
+			*/
+			display.Redisplay()
+
+			// Now wait for and handle user event
+			ev := display.Screen().PollEvent()
+			switch ev := ev.(type) {
+			case *tcell.EventKey:
+				switch ev.Key() {
+				case tcell.KeyEscape, tcell.KeyEnter, tcell.KeyCtrlQ:
+					close(quit)
+					return
+				default:
+					if ev.Key() == tcell.KeyRune {
+						world.CurrentBuffer().InsertChar(ev.Rune())
+						display.Redisplay()
+					}
+				}
+			case *tcell.EventResize:
+				display.Refresh()
+			}
+		}
+	}()
+
+	<-quit
+
+	err = display.End()
+	if err != nil {
+		Fatal(err)
+	}
+}
+
+func Fatal(err error) {
+	fmt.Fprintf(os.Stderr, "%v\n", err)
+	os.Exit(1)
+}
